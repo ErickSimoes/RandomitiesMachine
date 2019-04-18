4c3af269cec47841f44caad7635e6dae06d2b481
Erick Simões <erick.simoes@live.com>
2019-04-18 11:18:29 -0300

    Add TODO comments of future features

--- RandomitiesMachine.ino
+++ RandomitiesMachine.ino
@@ -1,6 +1,7 @@
 #define BUTTON 2
 #define LIST_SIZE 5
 
+//TODO: Add SD Card to populate the list
 String list[LIST_SIZE] = {"Chocolate",
                           "Banana",
                           "Lasanha",
@@ -10,10 +11,12 @@ String list[LIST_SIZE] = {"Chocolate",
 void setup() {
   Serial.begin(9600);
   pinMode(BUTTON, INPUT);
+  //TODO: Implement non repeat algorithm
 }
 
 void loop() {
   if(digitalRead(BUTTON)){
+    //TODO: Show the result in a LCD
     Serial.println(list[random(0, LIST_SIZE)]);
     delay(100);
   }
