3d55f574c918513b111801caa0b12750764cc605
Erick Simões <erick.simoes@live.com>
2019-04-18 11:13:50 -0300

    Add base algorithm to display a random element from a list
    By now, the element is displayed on the Serial Monitor.

--- RandomitiesMachine.ino
+++ RandomitiesMachine.ino
@@ -1,7 +1,20 @@
+#define BUTTON 2
+#define LIST_SIZE 5
+
+String list[LIST_SIZE] = {"Chocolate",
+                          "Banana",
+                          "Lasanha",
+                          "Pizza",
+                          "Goiabada"};
+
 void setup() {
-  
+  Serial.begin(9600);
+  pinMode(BUTTON, INPUT);
 }
 
 void loop() {
-  
+  if(digitalRead(BUTTON)){
+    Serial.println(list[random(0, LIST_SIZE)]);
+    delay(100);
+  }
 }
